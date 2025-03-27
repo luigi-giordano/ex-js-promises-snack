3e9bfa6207cb0b064e258ea7a2a25cd586d490ba
luigi-giordano <luigi.giordano1992@gmail.com>
2025-03-27 11:01:39 +0100

    [fix ex]

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 // Ottieni il titolo di un post con una Promise.
 
 // Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
-// 🎯 Bonus: Ottieni l'intero post con l'autore
-// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
 
 function getPostTitle(id) {
   return new Promise((resolve, reject) => {
@@ -15,8 +13,11 @@ function getPostTitle(id) {
 }
 
 getPostTitle(1)
-  .then(response => console.log(response))
+  .then(title => console.log(title))
   .catch(error => console.error(error))
+
+// 🎯 Bonus: Ottieni l'intero post con l'autore
+// Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
 
 function getPost(id) {
   return new Promise((resolve, reject) => {
@@ -25,10 +26,7 @@ function getPost(id) {
       .then(post => {
         fetch(`https://dummyjson.com/users/${post.userId}`)
           .then(response => response.json())
-          .then(user => {
-            post.user = user
-            resolve(post)
-          })
+          .then(user => resolve({ ...post, user }))
           .catch(reject)
       })
       .catch(reject)
@@ -41,8 +39,6 @@ getPost(1)
 
 // Snack 2
 // Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
-// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
-// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
 
 // function lanciaDado() {
 //   return new Promise((resolve, reject) => {
@@ -56,9 +52,12 @@ getPost(1)
 //       }
 //     }, 3000);
 //   });
-// }
+// } 
 
+// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
 function creaLanciaDado() {
+
   let ultimoRisultato = null;
 
   return function lanciaDado() {
